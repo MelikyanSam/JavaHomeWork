a85b660eed7e9e1013eaff27697689530057aa5e
MelikyanSam <samvel.melikyan01@mail.ru>
2022-04-17 19:43:12 +0400

    HomeWork-7: Constructors.

--- src/com/company/HomeWork/Work7/Constructor.java
+++ src/com/company/HomeWork/Work7/Constructor.java
@@ -8,6 +8,10 @@ public class Constructor {
         stck = new int[num];
         tos = -1;
     }
+    Constructor(){
+        stck = new int[10];
+        tos = -1;
+    }
 
     void push (int item){
         if(tos == stck.length-1)
@@ -28,6 +32,7 @@ public class Constructor {
     public static void main(String[] args) {
         Constructor myStack = new Constructor(100);
         Constructor myStack1 = new Constructor(46);
+        Constructor myStack2 = new Constructor();
 
 
         for(int i = 1; i < myStack.stck.length; i++){
@@ -36,12 +41,18 @@ public class Constructor {
         for(int i = 1; i < myStack1.stck.length; i++){
             myStack1.push(i);
         }
+        for(int i = 1; i < myStack2.stck.length; i++){
+            myStack2.push(i);
+        }
 
         for(int i = 1; i < myStack.stck.length; i++){
             System.out.println(myStack.pop());
         }
         for(int i = 1; i < myStack1.stck.length; i++){
             System.out.println(myStack1.pop());
+        }
+        for(int i = 1; i < myStack2.stck.length; i++){
+            System.out.println(myStack2.pop());
         }
     }
 }
